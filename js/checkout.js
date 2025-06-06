6268f3a9d2a6f924b4d4366b7a4f04bf5ee5b1be
maleesh <126404799+SaaiKrahaanth@users.noreply.github.com>
2025-06-06 09:43:59 +0530

    checkout design completion

--- js/checkout.js
+++ js/checkout.js
@@ -6,16 +6,18 @@ function loadCart(){
     let total = 0;
     cartItems.forEach(product => {
         const col = document.createElement("div");
-        col.className = "col-md-4 mb-4";
-        col.innerHTML = `
-            <div class="card h-100">
-<img src="${product.image}" class="card-img-top" style="height:200px; object-fit:cover;" alt="${product.name}">
-                <div class="card-body">
-                 <h5 class="card-title">${product.name}</h5>
-                    <p class="card-text text-success fw-bold">$${product.price.toFixed(2)}</p>
-                </div>
-                `;
-                cartContainer.appendChild(col);
+col.className = "col-md-4 mb-4";
+col.innerHTML = `
+    <div class="card h-100">
+        <img src="${product.image}" class="card-img-top" alt="${product.name}">
+        <div class="card-body">
+            <h5 class="card-title">${product.name}</h5>
+            <p class="card-text text-success fw-bold">$${product.price.toFixed(2)}</p>
+        </div>
+    </div>
+`;
+cartContainer.appendChild(col);
+
         total += product.price; // Add product price to total
     });
 totalAmount.textContent = total.toFixed(2);
